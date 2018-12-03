c8d0097ee5c58c2e098d481676658ce0629c1452
enzoarata <enzoarata50@gmail.com>
2018-12-02 18:09:36 -0800

    Router class added

---
+++ Router.cpp
@@ -0,0 +1,36 @@
+#include "Router.h"
+
+Router::Router(){
+	routerID = -1;
+	buffer = 0;
+	transmissionDelay = 0;
+	propagationSpeed = 0;
+	processingDelay = 0;
+	bandwidth = 0;
+	lossChance = 0;
+	isFull = false;
+	abilityToRoute = false;
+
+}
+
+
+Router::Router(int id, int buffSize, double transmissionDly, double propagationDly, double processingDly, double band, double loss){
+
+	routerID = id;
+	buffer = buffSize;
+	transmissionDelay = transmissionDly;
+	propagationSpeed = propagationDly;
+	processingDelay = processingDly;
+	bandwidth = band;
+	lossChance = loss;
+}
+
+void Router::addConnection(Router newConnection, int length){
+	pair<Router, int> tempConnection;
+	tempConnection.first = newConnection;
+	tempConnection.second = length;
+	connections.push_back(tempConnection);
+}
+
+
+
---
+++ Router.h
@@ -0,0 +1,25 @@
+#include <vector.h>
+
+using namespace std;
+
+class Router{
+
+	private:
+		int routerID;
+		int buffer;
+		vector<pair<Router, int> > connections;
+		double transmissionDelay;
+		double propagationSpeed;
+		double processingDelay;
+		double bandwidth;
+		double lossChance;
+		bool isFull;
+		bool abilityToRoute;
+	public:
+		Router();
+		Router(int id, int buffSize, double transmissionDly, double propagationDly, double processingDly, double band, double loss);
+		~Router();
+		void addConnection(Router newConnection, int length);
+		int getID();
+		
+};
